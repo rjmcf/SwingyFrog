2134212271d3c1cd19e6ab9b4423d7651990c293
oshannonlepper <oshannonlepper@gmail.com>
2019-01-26 23:36:27 +0000

    Refactored chain link prefab.

---
+++ Assets/ChainLinkComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HingeJoint2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class ChainLinkComponent : MonoBehaviour {
+
+	private HingeJoint2D _hingeJoint = null;
+	private Rigidbody2D _rigidbody = null;
+
+	public HingeJoint2D HingeJoint
+	{
+		get
+		{
+			return _hingeJoint;
+		}
+	}
+
+	public Rigidbody2D Rigidbody
+	{
+		get
+		{
+			return _rigidbody;
+		}
+	}
+
+	private void Awake()
+	{
+		_hingeJoint = GetComponent<HingeJoint2D>();
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+}
--- Assets/Scripts/TongueCreater.cs
+++ Assets/Scripts/TongueCreater.cs
@@ -8,10 +8,48 @@ public class TongueCreater : MonoBehaviour
 	public List<Transform> TongueSegments;
 	public Rigidbody2D Ceiling;
 
-	[SerializeField] private GameObject ChainLinkOriginPrefab;
+	//[SerializeField] private GameObject ChainLinkOriginPrefab;
+	[SerializeField] private ChainLinkComponent ChainLinkPrefab;
 	[SerializeField] private float SegmentLength;
 
 	public void CreateTongue(Transform FrogTransform, Vector2 AttachPoint)
+	{
+		Vector2 FrogPoint = FrogTransform.position;
+		int Multiplier = AttachPoint.x > FrogPoint.x ? -1 : 1;
+		Vector2 SurfaceToFrog = FrogPoint - AttachPoint;
+		Vector2 SurfaceToFrogDir = SurfaceToFrog.normalized;
+		float NumSegments = SurfaceToFrog.magnitude / SegmentLength;
+		float CosAngleToRotate = Vector2.Dot(Vector2.up, -SurfaceToFrogDir);
+
+		List<Vector2> SegmentLocations = new List<Vector2>();
+		for (int i = 0; i < NumSegments - 1; i++)
+		{
+			SegmentLocations.Add(AttachPoint + SurfaceToFrogDir * SegmentLength * i);
+		}
+
+		Rigidbody2D LastRigidbody = null;
+		ChainLinkComponent CurrentSegment = null;
+		TongueSegments = new List<Transform>();
+
+		foreach (Vector2 SegmentLocation in SegmentLocations)
+		{
+			CurrentSegment = GameObject.Instantiate(ChainLinkPrefab, SegmentLocation, Quaternion.identity, transform);
+			TongueSegments.Add(CurrentSegment.transform);
+			CurrentSegment.transform.eulerAngles = new Vector3(0f, 0f, Multiplier * Mathf.Rad2Deg * Mathf.Acos(CosAngleToRotate));
+			if (LastRigidbody)
+			{
+				CurrentSegment.HingeJoint.connectedBody = LastRigidbody;
+			}
+			LastRigidbody = CurrentSegment.Rigidbody;
+		}
+
+		TongueSegments.Add(FrogTransform);
+		TongueSegments.Reverse();
+		GetComponent<TongueRenderer>().SetTargets(TongueSegments);
+		TongueAttachRigidbody = LastRigidbody;
+	}
+
+	/*public void CreateTongue(Transform FrogTransform, Vector2 AttachPoint)
 	{
 		Vector2 FrogPoint = FrogTransform.position;
 		int Multiplier = AttachPoint.x > FrogPoint.x ? -1 : 1;
@@ -42,5 +80,5 @@ public class TongueCreater : MonoBehaviour
 		TongueSegments.Reverse();
 		GetComponent<TongueRenderer>().SetTargets(TongueSegments);
 		TongueAttachRigidbody = CurrentSegment.GetComponentInChildren<Rigidbody2D>();
-	}
+	}*/
 }
