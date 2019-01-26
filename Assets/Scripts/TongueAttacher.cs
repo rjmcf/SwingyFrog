a4a3f993b58484f211410dc2f78000d9c433a5bc
Oli Shannon-Lepper (Laptop) <oshannonlepper@gmail.com>
2019-01-26 13:18:27 +0000

    merge

---
+++ Assets/Scripts/TongueAttacher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueAttacher : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+	{
+		HingeJoint2D Hinge = gameObject.GetComponent<HingeJoint2D>();
+		if (!Hinge.enabled && Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("New Attachment");
+		}
+	}
+}
