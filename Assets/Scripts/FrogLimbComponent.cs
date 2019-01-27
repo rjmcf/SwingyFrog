6d7552c4270b2d5b7b0fed129a7d6175c6772a2b
oshannonlepper <oshannonlepper@gmail.com>
2019-01-27 13:15:25 +0000

    Added floppy limbs.

---
+++ Assets/Scripts/FrogLimbComponent.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class FrogLimbComponent : MonoBehaviour
+{
+
+	[SerializeField] private ChainLinkComponent HingePrefab;
+
+	[Range(3, 20)]
+	[SerializeField] private int NumBones;
+	[SerializeField] private float ArmLength;
+	[SerializeField] private float ArmThickness;
+
+	private Rigidbody2D _rigidbody;
+	private LineRenderer _lineRenderer;
+
+	private Transform[] _bones;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_lineRenderer = GetComponent<LineRenderer>();
+
+		BuildLimb();
+	}
+
+	private GameObject MakeChain(string name)
+	{
+		GameObject obj = new GameObject(name);
+		Rigidbody2D rigidbody = obj.AddComponent<Rigidbody2D>();
+		rigidbody.mass = 2.0f;
+		rigidbody.sharedMaterial = _rigidbody.sharedMaterial;
+
+		BoxCollider2D collider = obj.AddComponent<BoxCollider2D>();
+		collider.size = Vector2.one * (ArmLength / NumBones);
+
+		HingeJoint2D hinge = obj.AddComponent<HingeJoint2D>();
+		hinge.autoConfigureConnectedAnchor = false;
+		hinge.anchor = Vector2.up * collider.size.y * 0.5f;
+		hinge.connectedAnchor = Vector2.down * collider.size.y * 0.5f;
+		hinge.enableCollision = true;
+
+		return obj;
+	}
+
+	void BuildLimb()
+	{
+		_bones = new Transform[NumBones];
+		Rigidbody2D previousBody = null;
+		_lineRenderer.positionCount = NumBones;
+		for (int boneIndex = 0; boneIndex < NumBones; ++boneIndex)
+		{
+
+			if (boneIndex == 0)
+			{
+				_bones[boneIndex] = transform;
+				previousBody = _rigidbody;
+			}
+			else
+			{
+				GameObject chain = MakeChain("Limb_" + boneIndex.ToString());
+				HingeJoint2D hinge = chain.GetComponent<HingeJoint2D>();
+				_bones[boneIndex] = chain.transform;
+
+				hinge.connectedBody = previousBody;
+				previousBody = chain.GetComponent<Rigidbody2D>();
+				_bones[boneIndex].transform.parent = _bones[boneIndex - 1].transform;
+
+				_bones[boneIndex].transform.localPosition = Vector3.right * (ArmLength*boneIndex/NumBones);
+				_bones[boneIndex].transform.localRotation = Quaternion.identity;
+			}
+
+			_lineRenderer.SetPosition(boneIndex, _bones[boneIndex].transform.position);
+		}
+	}
+
+	private void Update()
+	{
+		for (int boneIndex = 0; boneIndex < NumBones; ++boneIndex)
+		{
+			_lineRenderer.SetPosition(boneIndex, _bones[boneIndex].position);
+		}
+	}
+
+}
