b93cd9548c66093b99a5850dd4c91656f03af897
rjmcf <rjmcf@live.co.uk>
2019-01-27 16:40:27 +0000

    Test Trigger

---
+++ Assets/Scripts/Character/Tongue/TongueManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueManager : MonoBehaviour {
+
+	// Destroy everything that enters the trigger
+   void OnTriggerEnter(Collider other)
+   {
+	   Debug.Log("Collider Entered");
+	   Debug.Log(gameObject);
+   }
+}
